965b3b6b1388a868c24980ac107dd437f9754588
juliasankina1990-debug <juliasankina1990@gmail.com>
2026-04-21 09:13:51 +0300

    Remove trailing whitespace in Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -14,14 +14,14 @@ enum class STATES {
 };
 
 class Automata {
- private:  
+ private:
     int cash;
     std::vector<std::string> menu;
     std::vector<int> prices;
     STATES state;
     int selectedDrink;
 
- public: 
+ public:
     Automata();
     void on();
     void off();
